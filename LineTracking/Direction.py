67dad9fa6fbcd7a074567805935a74feaca4acd8
enoch248 <enochisveryrandomtoo@gmail.com>
2021-07-19 17:39:15 +0800

    19th Jul

--- LineTracking/Direction.py
+++ LineTracking/Direction.py
@@ -1,11 +1,33 @@
 import time
-import status
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+import Adafruit_PCA9685
+
 status = 0
 
 # Constants
 speed = 250
 timethreshold = 600 # 10 mins
-weightthreshold = (weight of two trays)
+weightthreshold = 4000
+
+# Hardware SPI configuration:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+# Initialise the PCA9685 using the default address (0x40).
+pwm = Adafruit_PCA9685.PCA9685()
+
+
+def debug(): 
+    values = [0]*8
+    for i in range(8):
+        # The read_adc function will get the value of the specified channel (0-7).
+        values[i] = mcp.read_adc(i)
+    # Print the ADC values.
+    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+    # Pause for half a second.
+    time.sleep(0.5)
+
 
 def left():
     pwm.set_pwm(0, 0, 0) # Direction L
@@ -43,44 +65,41 @@ def still():
     pwm.set_pwm(2, 0, 0) # Direction R
     pwm.set_pwm(3, 0, 0) # Speed R
 
-x = 0
-
 def isEndOfTrack():
-    return ((mcp.read_adc(3) == 0) and (mcp.read_adc(4) == 0))
+    # Kelvin: Middle two sensors white? Will there be a chance that this condition
+    # is also satisfied when the robot is going straight? Perhaps detecting all 8
+    # sensors black would be a better idea?
+    return (matches_pattern('BBBBBBBB'))
 
 def onTrack():
-    return ((mcp.read_adc(2) == 0) and (mcp.read_adc(5) == 0))
+    # Kelvin: 2nd from middle white, and 1st from middle black?
+    return (matches_pattern('**0BB0**'))
 
+# Kelvin: left90 and leftsensor same pattern?
 def left90():
-    return ((mcp.read_adc(5) != 0) and (mcp.read_adc(6) != 0) and (mcp.read_adc(7) != 0))
+    
+    return (matches_pattern('BBBBB000'))
 
 def right90():
-    return ((mcp.read_adc(0) != 0) and (mcp.read_adc(1) != 0) and (mcp.read_adc(2) != 0))
+    return (matches_pattern('000BBBBB'))
 
 def leftsensor():
-    return ((mcp.read_adc(5) != 0) or (mcp.read_adc(6) != 0) or (mcp.read_adc(7) != 0))
+    return (matches_pattern('**B*****'))
 
 def rightsensor():
-    return ((mcp.read_adc(0) != 0) or (mcp.read_adc(1) != 0) or (mcp.read_adc(2) != 0))
+    return (matches_pattern('*****B**'))
 
 def loadpattern():
-    return ((mcp.read_adc(0) != 0) and (mcp.read_adc(1) != 0) and (mcp.read_adc(2) != 0) and (mcp.read_adc(3) == 0) and (mcp.read_adc(4) == 0) and (mcp.read_adc(5) != 0) and (mcp.read_adc(6) != 0) and (mcp.read_adc(7) != 0))
-
+    return (matches_pattern('BBB00BBB'))
+    
 def timer():
     time.sleep(timethreshold)
     return True
 
         
 # 7 is L, 0 is R
-# White colour: mcp.read_adc(x) = 0
-# Black colour: mcp.read_adc(x) != 0
 
-# Status:
-# 0 = normal
-# 1 = unload
-# 2 = main station
-# 3 = reload
-# 4 = merge
+
     
 # Direction = 0 is to lengthen actuator
 # Direction = 4095 is to lower actuator    
@@ -96,28 +115,38 @@ def increaseactuator():
 def stopactuator():
     pwm.set_pwm(4, 0, 0) # Speed Linear Actuator
     pwm.set_pwm(5, 0, 0) # Direction Linear Actuator
-
-def docking():
-
-    # Main Station
-    if loadpattern():
-        still()
-        loweractuator()
-        time.sleep(5)
-        stopactuator()
-        break
-    else:
-        movement()
     
-    # Reload
+# Status:
+# 0 = normal
+# 1 = unload
+# 2 = reload
+# 3 = merge
+
+def docking(): 
+    # Main Station
+    # Kelvin: Why is there two load pattern? 
     if loadpattern():
-        still()
-        increaseactuator()
-        time.delay(5)
-        stopactuator()
-        break
+        if status == 1: # Unloading
+            still()
+            loweractuator()
+            time.sleep(5)
+            stopactuator()
+            status = 2
+        elif status == 2: # Loading
+            still()
+            increaseactuator()
+            time.delay(5)
+            stopactuator()
+            status = 0
+            return None
+    elif leftsensor(): 
+        left()
+    elif rightsensor():
+        right()
     else:
-        movement()
+        straight()
+        
+        
         
 
 def sensorcheck():
@@ -126,19 +155,49 @@ def sensorcheck():
 def movement():
     if isEndOfTrack():
         exit()
-    elif (left90() and sensorcheck()): 
-        still()
-        hardright()
-        time.sleep(7)
-        docking()
-        continue
-    elif (right90()): 
+    elif (left90() and sensorcheck()):
+        hardleft()
+        if (matchpattern('*****B**') == True):
+            status = 1
+            docking()        
+    elif right90(): 
+        # Kelvin: where would you encounter this condition?
         hardright()
     elif leftsensor(): 
         left()
     elif rightsensor():
         right()
-    else onTrack():
+    else:
         straight()
+
+def matches_pattern(pattern):
+    # ========================================================
+    # Returns true if pattern indicated is matched
+    # B = Black, O = White, * = Anything (don't care)
+    #
+    # Examples:
+    #    [*****BBB]: Left90 pattern
+    #    [BBB*****]: Right90 pattern
+    # 
+    # Usage:
+    #    if matches_pattern([*****BBB]):
+    #        still()
+    #        hardright()
+    #        ...
+    # ========================================================
+    assert len(pattern) == 8, 'Patterns should have length of 8'
+
+    for i, p in enumerate(pattern):
+        if p == '*':
+            continue
+        elif (p == 'B' and (mcp.read_adc(i) < 600)):
+            # If reading is white, return False because pattern does not match
+            return False
+        elif (p == 'O' and (mcp.read_adc(i) > 900)):
+            return False
+
+    return True
+
+
         
 
