f6ea45cf381477edfc9bc341265d0f925c57b395
enoch248 <enochisveryrandomtoo@gmail.com>
2021-07-21 16:39:52 +0800

    21.7 FINAL

--- LineTracking/Combine.py
+++ LineTracking/Combine.py
@@ -1,11 +1,19 @@
 import Direction
-from Direction import movement, debug
+from Direction import movement, debug, still
 from Status import Status
+import ProximitySensor
+from ProximitySensor import tooClose
+import time
 
 if __name__ == "__main__":
     x = 0
-    status = Status.NORMAL
+    Status = Status.NORMAL
 
     while True:
-        debug()
-        status = movement(status)
+        if (tooClose() != True):
+            Status = movement(Status)
+            
+        else:
+            still()
+            time.sleep(3) # Delay 3s until it checks again
+
