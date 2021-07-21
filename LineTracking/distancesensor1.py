f99b83c43492182d25799cdea4f2f47a81a9a2fe
enoch248 <enochisveryrandomtoo@gmail.com>
2021-07-21 15:19:55 +0800

    21.7 update v2

--- LineTracking/distancesensor1.py
+++ LineTracking/distancesensor1.py
@@ -1,12 +1,13 @@
 import time
 import board
 import adafruit_hcsr04
+import RPi.GPIO as GPIO
 
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D1, echo_pin=board.D19)
 
 while True:
     try:
-        print((sonar.distance,))
+        print(sonar.distance)
     except RuntimeError:
         print("Retrying!")
     time.sleep(2)
