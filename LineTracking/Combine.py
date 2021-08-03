5dc0f88496fa9bfc0541a79e358603c8c06da627
enoch248 <enochisveryrandomtoo@gmail.com>
2021-08-03 16:14:37 +0800

    3.8 update

--- LineTracking/Combine.py
+++ LineTracking/Combine.py
@@ -1,22 +1,38 @@
+import RPi.GPIO as GPIO
 import Direction
 from Direction import movement, debug, still
 from Status import Status
 import ProximitySensor
 from ProximitySensor import tooClose
 import time
-from WeightSensor import HX711
 
 
 
-if __name__ == "__main__":
-    x = 0
-    Status = Status.NORMAL
-    t = 300
-    while True:
-        if (tooClose() == True):
-            still()
-            time.sleep(3) # Delay 3s until it checks surroundings again
-        else:
+try: 
+    while True: 
+        if __name__ == "__main__":
+            x = 0
+            Status = Status.NORMAL
+            t = 300
+            
+            #if (tooClose() == True):
+                #still()
+                #print("Someone is nearby")
+                #time.sleep(3) # Delay 3s until it checks surroundings again
+                
+            #else:
+            debug()
             Status = movement(Status)
+            print("Running")
+
+except KeyboardInterrupt:
+    print("KeyboardInterrupt")
+    still()
+
+finally:
+    GPIO.cleanup()
+    print("\nCleaning up!!")
+    quit()
+
             
 
